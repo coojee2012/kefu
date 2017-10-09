6f6f562908281fa151220cd565cb3aa2df00a070
Yong Lin <11366846@qq.com>
2017-10-09 10:40:22 +0800

    添加一个属性指令Demo

---
+++ www/src/app/directives/MyHighlight.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+@Directive({
+    selector: '[appHighlight]'
+})
+
+export class MyHighlightDirective {
+    @Input() defaultColor: string;
+    @Input() highlightColor: string;
+    constructor(private el: ElementRef) {
+
+    }
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight(this.highlightColor || this.defaultColor || 'red');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight(null);
+    }
+
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+
+}
+
